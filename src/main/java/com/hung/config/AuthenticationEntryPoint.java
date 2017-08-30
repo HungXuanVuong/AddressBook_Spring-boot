461d98f0b4dddd56f9e6b47f9498ba97a32aac23
ANH-HUNG <ANH-HUNG@192.168.1.17>
2017-08-30 20:50:54 +0700

    add spring security basic

--- src/main/java/com/hung/config/AuthenticationEntryPoint.java
+++ src/main/java/com/hung/config/AuthenticationEntryPoint.java
@@ -17,15 +17,15 @@ public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
-		response.addHeader("WWW-Authenticate", "Basic realm - " + getRealmName());
+		response.addHeader("WWW-Authenticate", "Basic realm -" + getRealmName());
 		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 		PrintWriter writer = response.getWriter();
-		writer.println("Http Status 401 "+authException.getMessage());
+		writer.println("Http Status 401 " + authException.getMessage());
 	}
-	
+
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		setRealmName("hung"); 
+		setRealmName("Hung");
 		super.afterPropertiesSet();
 	}
 }
--- src/main/java/com/hung/config/SpringSecurityConfiguration.java
+++ src/main/java/com/hung/config/SpringSecurityConfiguration.java
@@ -3,47 +3,30 @@ package com.hung.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import com.hung.service.impl.MyUserDetailService;
-
-
-
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Autowired
-	private AuthenticationEntryPoint entryPoint;
-
+	AuthenticationEntryPoint entryPoint;
+	
 	@Autowired
-	private MyUserDetailService userDetailsService;
-
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.userDetailsService(userDetailsService);
-//	}
-//
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication().withUser("1").password("1").roles("USER");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().httpBasic()
+		.authenticationEntryPoint(entryPoint);
+	}
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests().anyRequest().authenticated().and().httpBasic().authenticationEntryPoint(entryPoint);
+//		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
 //	}
-
-	/*
-	 * @Autowired public void configureGlobal(AuthenticationManagerBuilder auth)
-	 * throws Exception {
-	 * auth.inMemoryAuthentication().withUser("user").password("password").roles
-	 * ("USER"); }
-	 */
-
-	/*
-	 * @Override protected void configure(HttpSecurity http) throws Exception {
-	 * http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
-	 * }
-	 */
-
+	
 }
